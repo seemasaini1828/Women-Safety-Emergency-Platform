ee1de0a0375b48d0c05fd2a66ea92fb9e25d4034
Seema Saini <sainiseema@gmail.com>
2026-06-29 17:29:38 +0530

    Added contacts management page UI

--- fronted/Contacts.js
+++ fronted/Contacts.js
@@ -1,85 +1,22 @@
 function addContact(){
-    
-const userEmail =
-document.getElementById("userEmail").value;
 
-const name =
-document.getElementById("name").value;
+    const name = document.getElementById("cname").value;
+    const phone = document.getElementById("cphone").value;
 
-const phone =
-document.getElementById("phone").value;
+    if(!name || !phone){
+        alert("Please fill all fields");
+        return;
+    }
 
-const relation =
-document.getElementById("relation").value;
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+        <h3>${name}</h3>
+        <p>${phone}</p>
+    `;
 
-fetch("https://women-safetd-eyq2.onrender.comy-backen/add-contact",{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-userEmail,
-name,
-phone,
-relation
-})
-})
-.then(res=>res.json())
-.then(data=>{
-alert(data.message);
-});
+    document.getElementById("contactList").appendChild(div);
 
+    document.getElementById("cname").value = "";
+    document.getElementById("cphone").value = "";
 }
-
-function deleteContact(id){
-
-fetch(`https://women-safety-backend-eyq2.onrender.com/contact/${id}`,{
-method:"DELETE"
-})
-.then(res=>res.json())
-.then(data=>{
-
-alert(data.message);
-
-loadContacts();
-
-});
-
-}
-
-function loadContacts(){
-
-fetch("https://women-safety-backend-eyq2.onrender.com/contacts")
-.then(res=>res.json())
-.then(data=>{
-
-let output = "";
-
-data.forEach(contact=>{
-
-output += `
-<p>
-${contact.name}
-<br>
-${contact.phone}
-<br>
-${contact.relation}
-<br><br>
-
-<button onclick="deleteContact('${contact._id}')" id="deleteContactBtn">
-Delete Contact
-</button>
-
-</p>
-<hr>
-`;
-
-});
-
-document.getElementById("contacts").innerHTML = output;
-
-});
-
-}
-
-loadContacts();
